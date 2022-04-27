5e3a9b8166f5d07729aef59b02047be33ab4435f
ahnongji <102713148+ahnongji@users.noreply.github.com>
2022-04-28 00:00:24 +0900

    Add files via upload

---
+++ 5주차_추가/10.js
@@ -0,0 +1,11 @@
+let student = [90, 70, 80, 60];
+
+let student1 = {
+    koreanScore:90,
+    englisgScore:70,
+    mathScore:80,
+    scienceScore:60
+};
+
+console.log(student1["koreanScore"]);
+console.log(student1.englishScore);
---
+++ 5주차_추가/13.js
@@ -0,0 +1,17 @@
+function sum(num1, num2){
+    return num1 + num2;
+}
+console.log(sum(15, 20));
+
+const sum_result_1 = sum(10, 20);
+console.log(sum_result_1);
+
+function sum(number1, number2){
+    return number1 + number2;
+}
+
+const sum_result_3 = sum(10, 20);
+const sum_result_4 = sum(20, 30);
+
+const sum_result2 = sum_result_3 + sum_result_4; 
+console.log(sum_result2);
